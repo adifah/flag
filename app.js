37e5522ce69004bb709412ddbf09344e1ed2feba
Adrian F <fahle@upb.de>
2012-06-08 21:58:22 +0000

    moved everyauth code from app.js to new file auth.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 
 var express = require('express')
   , everyauth = require('everyauth')
+  , auth = require('./auth')
   , conf = require('./conf')
-  , db = require('dirty')('user.db') // read 'user.db' file into memory or create one if not present
   , routes = require('./routes')
   , logger = require('winston')
   , loggly = require('winston-loggly');  // Requiring `winston-loggly` will expose `winston.transports.Loggly`
@@ -14,44 +14,6 @@ logger.add(logger.transports.File, conf.logger.file);
 logger.add(logger.transports.Loggly, conf.logger.loggly);
 
 var port = process.env.C9_PORT || process.env.PORT || 3000; // process.env.C9_PORT is for c9.io, process.env.PORT is for heroku, 3000 for everything else
-
-everyauth.everymodule // delivers the correct session for every http request
-    .findUserById( function (id, callback) {
-        console.log("search user: " + id);
-        callback(null, db.get(id));
-    })
-    .moduleErrback( function (err) {
-        logger.error("Internal error '" + err + "' on authentication: ");
-    });
-    
-everyauth.twitter
-    .consumerKey(conf.twit.consumerKey)
-    .consumerSecret(conf.twit.consumerSecret)
-    .moduleErrback( function (err) {
-        console.log("Internal error '" + err + "' on authentication: ");
-    })
-    .findOrCreateUser( function (sess, accessToken, accessSecret, twitUser) {
-        // check if a user with this twitter id already exists in tht db
-        var user = db.get(twitUser.id);
-        if(user === undefined) {
-            console.log("create user: " + twitUser.id);
-            db.set(twitUser.id, createUser('twitter', twitUser));
-            user = db.get(twitUser.id);
-        }
-        var logins = ++user.logins;
-        db.set(twitUser.id, {logins : logins});
-        console.log("return user: " + twitUser.id + " (" + user.logins + " logins)");
-        return user;
-    })
-    .redirectPath('/dashboard');
-
-function createUser(source, sourceUser) {
-    var user = {};
-    user['id'] = sourceUser.id;
-    user[source] = sourceUser;
-    user['logins'] = 0;
-    return user;
-}
 
 everyauth.debug = false;
 
